75c31532cc40008a8271d17266b17c0b3c371e21
jinwoo22 <jenny010925@naver.com>
2025-05-03 15:05:40 +0900

    feat: 서버내 json 파일 저장

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import json
-from datetime import datetime
+import os
 from crawling_restaurants import crawling_restaurants
 from crawling_restaurnats_detail import crawling_restaurant_info
 from driver import get_driver
+
+save_dir = "/home/ubuntu/crawling"
+filename = "restaurants.json"
+file_path = os.path.join(save_dir, filename)
 
 driver = get_driver()
 restaurants = crawling_restaurants(driver, "https://map.naver.com/p/search/충무로%20음식점?c=15.00,0,0,0,dh")
@@ -10,7 +14,11 @@ restaurants_info = crawling_restaurant_info(driver, restaurants)
 
 print("[크롤링 결과]")
 print(f"\n 최종 수집 식당 수: {len(restaurants)}개")
-print(json.dumps(restaurants_info, ensure_ascii=False, indent=2))
 
-end = datetime.now()
+restaurants_json = json.dumps(restaurants_info, ensure_ascii=False, indent=2)
 driver.quit()
+
+with open(file_path, "w", encoding="utf-8") as f:
+    f.write(restaurants_json)
+
+print(f"저장완료 : {file_path}")
