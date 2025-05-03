d4662d6bd3bfe9adedc916885cefd04ce299e107
jinwoo22 <jenny010925@naver.com>
2025-05-03 15:08:03 +0900

    feat: 서버에 필요한 드라이버 설정 추가

--- driver.py
+++ driver.py
@@ -4,4 +4,7 @@ from selenium.webdriver.chrome.options import Options
 
 def get_driver():
     options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     return webdriver.Chrome(options=options)
