d1df146936a71aaf5a6a9b2297503fa829a15b71
jinwoo22 <jenny010925@naver.com>
2025-06-01 03:59:18 +0900

    feat: 데이터 중복 제거하고 저장

--- crawling_restaurnats_detail.py
+++ crawling_restaurnats_detail.py
@@ -53,12 +53,13 @@ def crawling_restaurant_info(driver, restaurant_elements):
 
             driver.switch_to.default_content()
             WebDriverWait(driver, 60).until(EC.frame_to_be_available_and_switch_to_it((By.ID, "searchIframe")))
-
+            del restaurant
+            
         except Exception as e:
             driver.switch_to.default_content()
             WebDriverWait(driver, 60).until(EC.frame_to_be_available_and_switch_to_it((By.ID, "searchIframe")))
             continue
-
+            
     return restaurant_info
 
 def crawling_address(driver):
