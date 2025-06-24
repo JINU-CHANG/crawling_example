acb5a92fd30cef00292b5f045c29e37ae8c82c54
jinwoo22 <jenny010925@naver.com>
2025-06-24 21:25:26 +0900

    refactor: 불필요한 파일 삭제

--- driver.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-def get_driver():
-    options = Options()
-    options.add_argument("--incognito")
-    return webdriver.Chrome(options=options)
